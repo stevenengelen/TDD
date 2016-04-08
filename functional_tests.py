cb863cc4ce53103b9da3a3448ff0cc45c863fa30
Steven <steven.engelen.home@gmail.com>
2016-04-08 17:27:03 +0200

    Functional tests now check we can enter a To-Do item.

--- functional_tests.py
+++ functional_tests.py
@@ -15,14 +15,19 @@ class NewVisitorTest(unittest.TestCase) :
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
-
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         # She enters a To-Do - "Buy peacock feathers".
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
         # She sees the To-Do
-
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
         # She can enter another To-Do - "Use peacock feathers to make a fly"
-
+        self.fail('Finish the test!')
         # She sees the 2 To-Do's
 
         # The site generates a unique URL
