24ac1da3e169a13ce648a76901d4ad42564a1ef3
Steven <steven.engelen.home@gmail.com>
2016-04-15 07:53:04 +0200

    Front page HTML now generated from template

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase) :
@@ -25,7 +26,7 @@ class NewVisitorTest(unittest.TestCase) :
         # She sees the To-Do
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table")
         # She can enter another To-Do - "Use peacock feathers to make a fly"
         self.fail('Finish the test!')
         # She sees the 2 To-Do's
