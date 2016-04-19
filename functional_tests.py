d83949a0cc2cb186010975ee1c8190d51705a030
Steven <steven.engelen.home@gmail.com>
2016-04-19 17:17:48 +0200

    Add FT to test add items to the to-do list.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase) :
 
@@ -23,10 +24,15 @@ class NewVisitorTest(unittest.TestCase) :
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        # She enters another To-Do - "Use peacock feathers to make a fly".
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
         # She sees the To-Do
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table")
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
         # She can enter another To-Do - "Use peacock feathers to make a fly"
         self.fail('Finish the test!')
         # She sees the 2 To-Do's
