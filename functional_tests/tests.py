98aa241482a078d846450bd4b62eae91cf519d22
Steven <steven.engelen.home@gmail.com>
2017-01-13 07:37:51 +0100

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,17 +6,31 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from contextlib import contextmanager
 from selenium.webdriver.support.expected_conditions import staleness_of
 from selenium.webdriver.support.ui import WebDriverWait
+import sys
 
 class NewVisitorTest(StaticLiveServerTestCase) :
 
     def setUp(self) :
-        # self.browser = webdriver.Firefox()
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(3)
 
     def tearDown(self) :
         self.browser.quit()
 
+    @classmethod
+    def setUpClass(cls) :
+        for arg in sys.argv :
+            if 'liveserver' in arg :
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls) :
+        if cls.server_url == cls.live_server_url :
+            super().tearDownClass()
+        
     @contextmanager
     def wait_for_page_load(self, timeout = 50) :
         old_page = self.browser.find_element_by_tag_name('html')
@@ -38,7 +52,7 @@ class NewVisitorTest(StaticLiveServerTestCase) :
 
     def test_can_start_a_list_and_retrieve_it_later(self) :
         # Edith opens the TO-DO app
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -73,7 +87,7 @@ class NewVisitorTest(StaticLiveServerTestCase) :
         self.browser = webdriver.Chrome()
 
         # Francis does not see Edith's list in the browser.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -104,7 +118,7 @@ class NewVisitorTest(StaticLiveServerTestCase) :
 
     def test_layout_and_styling(self) :
         # Edith goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         # using i3, so calculating a terminal next to a browser window in the test below
         # self.browser.set_window_size(1024, 768)
 
