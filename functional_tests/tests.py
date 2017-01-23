e18a3706917758f1c986b74ea3ac1c20ce4d5362
Steven <steven.engelen.home@gmail.com>
2017-01-23 22:02:22 +0100

    comment out the teardown class method because we use a version of django higher than 1.8

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -26,17 +26,20 @@ class NewVisitorTest(StaticLiveServerTestCase) :
         super().setUpClass()
         cls.server_url = cls.live_server_url
 
+    '''
     @classmethod
     def tearDownClass(cls) :
+        print(cls.server_url)
         if cls.server_url == cls.live_server_url :
             super().tearDownClass()
-        
+    '''
+    '''    
     @contextmanager
     def wait_for_page_load(self, timeout = 50) :
         old_page = self.browser.find_element_by_tag_name('html')
         yield
         WebDriverWait(self.browser, timeout).until(staleness_of(old_page))
-
+    '''
     def check_for_row_in_list_table(self, row_text) :
         '''
         start_time = time.time()
