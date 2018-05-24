7ae1a374db14d19dbafb57aa86be20677064213c
Steven <steven.engelen.home@gmail.com>
2018-05-24 08:32:40 +0200

    refactored the list item validation test to the latest version of book

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -14,24 +14,32 @@ class ItemValidationTest(FunctionalTest) :
 
         # The home page refreshes, and there is an error message saying that list items
         # cannot be blank
-        error = self.wait_for(lambda : self.browser.find_element_by_css_selector('.has-error'))
-        # error = self.wait_for(lambda : self.browser.find_element_by_css_selector('#id_text:invalid'))
-        self.assertEqual(error.text, "You can't have an empty list item")
+        # error = self.wait_for(lambda : self.browser.find_element_by_css_selector('.has-error'))
+        self.wait_for(lambda : self.browser.find_element_by_css_selector('#id_text:invalid'))
+        # self.assertEqual(error.text, "You can't have an empty list item")
 
         # She tries again with some text for the item, which now works
-        self.get_item_input_box().send_keys('Buy milk\n')
-        self.check_for_row_in_list_table('1: Buy milk')
+        self.get_item_input_box().send_keys('Buy milk')
+        self.wait_for(lambda : self.browser.find_element_by_css_selector('#id_text:valid'))
+
+        # And she can submit it succesfully
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
 
         # Peversely, she now decides to submit a second blank list item
-        self.get_item_input_box().send_keys('\n')
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         # She receives a similar warning on the list page
-        self.check_for_row_in_list_table('1: Buy milk')
-        error = self.wait_for(lambda : self.browser.find_element_by_css_selector('.has-error'))
-        self.assertEqual(error.text, "You can't have an empty list item")
+        self.wait_for_row_in_list_table('1: Buy milk')
+        # self.check_for_row_in_list_table('1: Buy milk')
+        self.wait_for(lambda : self.browser.find_element_by_css_selector('#id_text:invalid'))
+        # error = self.wait_for(lambda : self.browser.find_element_by_css_selector('.has-error'))
+        # self.assertEqual(error.text, "You can't have an empty list item")
 
         # And she can correct it by filling some text in
-        self.get_item_input_box().send_keys('Make tea\n')
+        self.get_item_input_box().send_keys('Make tea')
+        self.wait_for(lambda : self.browser.find_element_by_css_selector('#id_text:valid'))
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
 
